8e02ae68577c397ce69be7054e0c8c3cc93f1d10
Anna Lichacz <anniahod@wp.pl>
2023-02-08 21:48:13 +0100

    Add js

--- js/script.js
+++ js/script.js
@@ -42,7 +42,55 @@ function titleClickHandler(event){
   targetArticle.classList.add('active');
 }
 
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(customSelector = ''){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  /* for each article */
+
+  const articles = document.querySelectorAll(optArticleSelector + customSelector);
+
+  let html = '';
+  
+  for(let article of articles){
+
+    /* get the article id */
+
+    const articleId = article.getAttribute('id');
+
+    /* find the title element */
+
+    const titleElement = article.querySelector(opts.titleSelector);
+
+    /* get the title from the title element */
+
+    const title = titleElement.innerHTML;
+
+    /* create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('linkHTML');
+
+    /* insert link into titleList */
+
+    html = html + linkHTML;
+    console.log(html);
+  }
+
+  titleList.innerHTML = html;
+}
+
+generateTitleLinks();
+
 const links = document.querySelectorAll('.titles a');
+console.log(links);
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
